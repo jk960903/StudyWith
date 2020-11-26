dc414fbc7dd3cd91e088ccae05cc5f5e1f510e8e
simbean <sb9338@naver.com>
2020-11-27 01:21:54 +0900

    i can't believe it

---
+++ Simbean/baek/main.py
@@ -0,0 +1,10 @@
+import s16112
+import s1463
+import s11399
+#s16112 : https://www.acmicpc.net/problem/16112
+#s1463 : https://www.acmicpc.net/problem/1463
+#s11399: https://www.acmicpc.net/problem/11399
+if __name__ == "__main__":
+    #s16112.main
+    #s11399.main
+    s1463.main()
