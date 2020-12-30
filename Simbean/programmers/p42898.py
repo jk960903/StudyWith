182b893163adc0d4d913e1eebc6dc260b74262f7
simbean <sb9338@naver.com>
2020-12-30 18:38:55 +0900

    20201230

--- Simbean/programmers/p42898.py
+++ Simbean/programmers/p42898.py
@@ -1,24 +1,24 @@
-#https://programmers.co.kr/learn/courses/30/lessons/42898
-#https://programmers.co.kr/learn/courses/30/lessons/42898
-#https://programmers.co.kr/learn/courses/30/lessons/42898
-def solution(m, n, puddles):
-    anslist = []
-    for i in range(0,m):
-        mlist = []
-        for j in range(0,n):
-            if [i+1,j+1] in puddles:
-                mlist.append(0)
-            elif i == 0 and j == 0:
-                mlist.append(1)
-            elif i == 0:
-                mlist.append(mlist[j-1])
-            elif j == 0:
-                mlist.append(anslist[i-1][j])
+def GCD(a,b):
+    if a%b == 0:
+        return b
+    else:
+        return -(a%b)
+def solution(arr):
+    answer = 1
+    min = 1
+    arr.sort()
+    c1 = arr[0]
+    temp = 0
+    for i in range(0,len(arr)-1):
+        c2 = arr[i+1]
+        while c1 >= 0:
+            if c1 > c2:
+                Temp = GCD(c1,c2)
+                c1 = c2
+                c2 = Temp
             else:
-                mlist.append(anslist[i-1][j] + mlist[j-1])
-        #print(mlist)
-        anslist.append(mlist)
-    answer = anslist[-1][-1]%1000000007
+                Temp = GCD(c2,c1)
+                c1 = c2
+                c2 = Temp
     return answer
 if __name__ == "__main__":
-    print(solution(4,3,[[2, 2]]))
