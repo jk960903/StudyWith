1ecdea6a70ca9ddd936ad43179f11b28b24cdb5c
simbean <sb9338@naver.com>
2021-01-03 17:01:39 +0900

    알고리즘 20210103까지 한것 1차 수정

--- Simbean/baek/b11723 집합.py
+++ Simbean/baek/b11723 집합.py
@@ -1,33 +1,30 @@
-def s2437_init():
-    global N,M
-    N = int(input())
-    M = input()
-    M = M.split()
-    M = list(map(int, M))
-    M.sort()
-def s2437_plus():
-    listA = []
-    sumM = 0
-    for i in range(0,len(M)):
-        sumM += M[i]
-        listA.append(sumM)
-    if M[0] != 1:
-        print(1)
-    else:
-        s2437_Compare(listA)
-def s2437_Compare(List):
-    sum = 0
-    aws = 0
-    for i in range(0, len(List)):
-        try:
-            if List[i]+1 < M[i+1]:
-                aws = List[i]+1
-                break
-        except IndexError:
-            aws = List[-1]+1
-    print(aws)
+import sys
+#https://www.acmicpc.net/problem/11723
+#https://www.acmicpc.net/problem/11723
 if __name__ == "__main__":
-    s2437_init()
-    #print(N)
-    #print(M)
-    s2437_plus()
+    b = 0
+    N = int(sys.stdin.readline())
+
+    t = 0
+    L = [0 for i in range(21)]
+
+    for i in range(0,N):
+        M = sys.stdin.readline().split()
+        if len(M) > 1:
+            index = int(M[1])
+            if M[0] == "add":
+                L[index] = 1
+            elif M[0] == "check":
+                print(L[index])
+            elif M[0] == "remove":
+                L[index] = 0
+            elif M[0] == "toggle":
+                if L[index] == 1:
+                    L[index] = 0
+                else:
+                    L[index] = 1
+        elif M[0] == "all":
+            L = [1 for i in range(21)]
+
+        elif M[0] == "empty":
+            L = [0 for i in range(21)]
