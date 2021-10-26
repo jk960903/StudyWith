c6bd6999f5f62db4b78432f8aadee88073c644e5
jk960903 <jk960903@naver.com>
2021-10-26 23:48:35 +0900

    20211026

---
+++ LJKAlgorithm/BaekJoon/src/생태학/Main.java
@@ -0,0 +1,38 @@
+package ปýลยวะ;
+import java.util.*;
+import java.io.*;
+public class Main {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		HashMap<String,Integer> map = new HashMap<>();
+		ArrayList<String> list = new ArrayList<>();
+		int total=0;
+		try {
+			String input = br.readLine();
+			while(true) {
+				if(map.containsKey(input)) {
+					map.put(input,map.get(input)+1);
+				}else {
+					map.put(input,1);
+					list.add(input);
+				}
+				total++;
+				input = br.readLine();
+				if(input==null || input.length()==0) {
+					break;
+				}
+			}
+			Collections.sort(list);
+			for(int i = 0; i<list.size(); i++) {
+				int num = map.get(list.get(i));
+				double percent = (double)num*100.0/(double)total;
+				System.out.printf(list.get(i)+" %.4f\n",percent);
+			}
+			br.close();
+		}catch(Exception e) {
+			
+		}
+		
+		
+	}
+}
