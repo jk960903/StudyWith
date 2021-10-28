f9ca2ad2414b182a30546b9aea73c47eee9179e4
jk960903 <jk960903@naver.com>
2021-10-28 23:38:04 +0900

    20211028

--- LJKAlgorithm/BaekJoon/src/크게만들기/Main.java
+++ LJKAlgorithm/BaekJoon/src/크게만들기/Main.java
@@ -6,25 +6,40 @@ public class Main {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st=new StringTokenizer(br.readLine()," ");
 		int n=Integer.parseInt(st.nextToken());
-		
+		int k = Integer.parseInt(st.nextToken());
+		String s = br.readLine();
+		Solution solution = new Solution();
+		System.out.println(solution.solution(s, k));
 	}
 }
 class Solution{
 	public String solution(String num,int k) {
-		String result="";
-		int[] array=new int[10];
-		for(int i=0; i<num.length(); i++) {
-			array[num.charAt(i)-'0']++;
+		StringBuilder sb=new StringBuilder();
+		Stack<Character> stack = new Stack<>();
+		int count=0;
+		stack.push(num.charAt(0));
+		for(int i = 1 ; i<num.length(); i++) {
+			char temp = num.charAt(i);
+			while(!stack.isEmpty()&&stack.lastElement() < temp && count<k) {
+				stack.pop();
+				count++;
+				if(count == k) break;
+			}
+			stack.push(temp);
 		}
-		int index=0;
-		int delete=0;
-		for(int i=0; i<10; i++) {
-			delete+=array[i];
-			//if(delete[])
+		int size = stack.size();
+		if(count<k) {
+			int gap = k - count;
+			for(int i = 0 ; i< size - gap; i++) {
+				sb.append(stack.get(i));
+			}
 		}
-		while(k!=0) {
-			
+		else {
+			for(int i = 0 ; i<size; i++) {
+				sb.append(stack.get(i));
+			}
 		}
-		return result;
+		
+		return sb.toString();
 	}
 }
