d313c975fa98932ecf2a3fa17d3f4a95350083f5
jk960903 <jk960903@naver.com>
2021-11-26 23:45:51 +0900

    20211126 말이되고픈원숭이

---
+++ LJKAlgorithm/BaekJoon/src/말이되고픈원숭이/Main.java
@@ -0,0 +1,90 @@
+package ¸»ÀÌµÇ°íÇÂ¿ø¼þÀÌ;
+import java.util.*;
+import java.io.*;
+public class Main {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			int k = Integer.parseInt(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine()," ");
+			
+			int w = Integer.parseInt(st.nextToken());
+			int h = Integer.parseInt(st.nextToken());
+			
+			int[][] map = new int[h][w];
+			for(int i = 0 ; i < h; i++) {
+				st = new StringTokenizer(br.readLine(), " ");
+				for(int j = 0 ; j < w; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			int answer = BFS(map,k);
+			System.out.println(answer);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static int BFS(int[][] map , int k) {
+		Queue<Monkey> queue = new LinkedList<>();
+		boolean[][][] visit = new boolean[k+1][map.length][map[0].length];
+		int answer = 0;
+		int[][] horseDir = {{-2,1},{1,2},{1,2},{2,1},{2,-1},{1,-2},{-1,-2},{-2,-1}};
+		int[][] dir = {{1,0},{0,1},{-1,0},{0,-1}};
+		queue.add(new Monkey(0,0,0,0));
+		visit[0][0][0] = true;
+		while(!queue.isEmpty()) {
+			Monkey m = queue.poll();
+			
+			if(m.x == map.length-1 && m.y == map[0].length-1) {
+				answer = m.time;
+				break;
+			}
+			if(m.horse < k) {
+				for(int i = 0 ; i < horseDir.length; i++) {
+					int tempx = horseDir[i][0] + m.x;
+					int tempy = horseDir[i][1] + m.y;
+					
+					if(inbound(tempx,tempy,map.length,map[0].length) && ! visit[m.horse][tempx][tempy] && 
+							map[tempx][tempy] != 1) {
+						queue.add(new Monkey(tempx,tempy,m.time+1,m.horse+1));
+						visit[m.horse+1][tempx][tempy] = true;
+					}
+				}
+			}
+			
+			for(int i = 0 ; i < dir.length; i++) {
+				int tempx = dir[i][0] + m.x;
+				int tempy = dir[i][1] + m.y;
+				
+				if(inbound(tempx,tempy,map.length,map[0].length) && !visit[m.horse][tempx][tempy] && map[tempx][tempy]!=1) {
+					queue.add(new Monkey(tempx,tempy,m.time+1,m.horse));
+					visit[m.horse][tempx][tempy] = true;
+				}
+			}
+		}
+		if(answer == 0 ) {
+			return -1;
+		}else return answer;
+	}
+	
+	private static boolean inbound(int tempx,int tempy, int x, int y) {
+		if(tempx >= 0 && tempy >= 0 && tempx < x && tempy < y) return true;
+		else return false;
+	}
+}
+
+class Monkey{
+	int x;
+	int y;
+	int time;
+	int horse;
+	
+	public Monkey(int x, int y, int time , int horse) {
+		this.x = x;
+		this.y = y;
+		this.time = time;
+		this.horse = horse;
+	}
+	
+}
--- LJKAlgorithm/BaekJoon/src/중량제한/Main.java
+++ LJKAlgorithm/BaekJoon/src/중량제한/Main.java
@@ -1,5 +1,72 @@
 package 중량제한;
 
+import java.util.*;
+import java.io.*;
 public class Main {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		try {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			City[] cities = new City[n+1];
+			//ArrayList<City>[] list = new ArrayList[n+1];
+			for(int i = 1; i < cities.length; i++) {
+				cities[i] = new City(i);
+			}
+			for(int i = 0; i<m; i++) {
+				st = new StringTokenizer(br.readLine(), " ");
+				int first = Integer.parseInt(st.nextToken());
+				int second = Integer.parseInt(st.nextToken());
+				int price = Integer.parseInt(st.nextToken());
+				City city1 = cities[first];
+				City city2 = cities[second];
+				if(city1.map.containsKey(second)) {
+					int tempprice = city1.map.get(second);
+					city1.map.put(second,Math.max(price,tempprice));
+				}else {
+					city1.map.put(second,price);
+					city1.set.add(second);
+				}
+				if(city2.map.containsKey(first)) {
+					int tempprice = city2.map.get(first);
+					city2.map.put(first,Math.max(tempprice, price));
+				}else {
+					city2.map.put(first,price);
+					city2.set.add(first);
+				}
+			}
+			st = new StringTokenizer(br.readLine()," ");
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			
+			Queue<City> queue = new LinkedList<>();
+			boolean[] visit = new boolean[n+1];
+			queue.add(cities[start]);
+			int max = 0 ;
+			
+			visit[start] = true;
+			
+			while(!queue.isEmpty()) {
+				City city = queue.poll();
+				
+				
+			}
+		}catch(Exception e) {
+			
+		}
+	}
+}
 
+class City{
+	int num;
+	HashMap<Integer,Integer> map;
+	HashSet<Integer> set;
+	
+	public City(int num) {
+		this.num = num;
+		this.map = new HashMap<>();
+		this.set = new HashSet<>();
+	}
 }
