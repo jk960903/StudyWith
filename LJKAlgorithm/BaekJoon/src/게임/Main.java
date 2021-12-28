7741ada9b3f1cc5d67dcba942c6c4f1acbedd801
jk960903 <jk960903@naver.com>
2021-12-28 23:35:12 +0900

    이분탐색 게임 준비

---
+++ LJKAlgorithm/BaekJoon/src/게임/Main.java
@@ -0,0 +1,13 @@
+package ∞‘¿”;
+import java.util.*;
+import java.io.*;
+public class Main {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			
+		}catch(Exception e) {
+			
+		}
+	}
+}
