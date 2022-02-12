b87c84c44062f31ca7d3532289ee526311e1833e
jk960903 <jk960903@naver.com>
2022-02-12 22:49:32 +0900

    20220212

--- LJKAlgorithm/BaekJoon/src/수들의합/Main.java
+++ LJKAlgorithm/BaekJoon/src/수들의합/Main.java
@@ -10,7 +10,43 @@ public class Main {
 		
 		StringTokenizer st = new StringTokenizer(br.readLine()," ");
 		
-		int i = 0 ; 
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine()," ");
+		
+		int[] arrays = new int[n];
+		
+		for(int i = 0; i < n ; i++) {
+			arrays[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		
+		int i = 0;
 		int j = 0;
+		
+		int count = 0 ;
+		int sum = arrays[0];
+		
+		
+		while(i < n) {
+			if(sum < m) {
+				j++;
+				if(j == arrays.length) {
+					break;
+				}
+				sum += arrays[j];
+			}else if(sum > m) {
+				sum -= arrays[i];
+				i++;
+			}else {
+				sum-= arrays[i];
+				i++;
+				count++;
+			}
+		}
+		
+		System.out.println(count);
 	}
 }
