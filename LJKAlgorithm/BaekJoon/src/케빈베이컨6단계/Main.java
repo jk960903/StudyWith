295cab992ce6a36599b6c34602212abb556707f6
jk960903 <jk960903@naver.com>
2022-03-05 22:25:55 +0900

    20220305

---
+++ LJKAlgorithm/BaekJoon/src/케빈베이컨6단계/Main.java
@@ -0,0 +1,29 @@
+package 케빈베이컨6단계;
+
+import java.util.*;
+import java.io.*;
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		List<List<Integer>> lists = new ArrayList<>();
+		lists.add(new ArrayList<>());
+		for(int i = 0 ; i < n ; i ++) {
+			lists.add(new ArrayList<>());
+		}
+		for(int i = 0 ; i < m ; i ++) {
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			
+			List<Integer> startList = lists.get(start);
+			startList.add(end);
+			
+			List<Integer> endList = lists.get(end);
+			startList.add(start);
+		}
+	}
+}
