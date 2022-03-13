a35e34657f26f6cefcf0425b9074dac47a26ae2d
jk960903 <jk960903@naver.com>
2022-03-13 23:44:34 +0900

    20220313

--- LJKAlgorithm/Data_Structure/Problem1.java
+++ LJKAlgorithm/Data_Structure/Problem1.java
@@ -1,44 +1,73 @@
-
 import java.io.*;
+import java.util.*;
 public class Problem1 {
 	public static void main(String[] args) {
-		for(Object k : System.getProperties().keySet()) {
-			String key = k.toString();
-			String value = System.getProperty(key);
-		}
-		try {
-			String path = System.getProperty("user.dir"); // 현재 디렉토리 가져오기
-			File file = new File(path+"\\property.html");
-			BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-			writer.write("<!DOCTYPE html>\n");
-			writer.write("<html charset=\"en\"\n");
-			writer.write("<head>\n");
-			writer.write("<meta charset=\"UTF-8\"/>");
-			writer.write("<style>\n table{ border-collapse : collapse : width : 100%; }"
-					+ "th, td { border : solid 1px #000; }\n");
-			writer.write("</style>");
-			writer.write("</head>\n");
-			writer.write("<body>\n");
-			writer.write("<table>\n");
-			writer.write("<tr>\n");
-			writer.write("<th>키</th>\n");
-			writer.write("<th></th>\n");
-			writer.write("</tr>\n");
-			for(Object k : System.getProperties().keySet()) {
-				writer.write("<tr>\n");
-				String key = k.toString();
-				String value = System.getProperty(key);
-				writer.write("<td>"+key+"</td>\n");
-				writer.write("<td>"+value+"</td>\n");
-				writer.write("</tr>\n");
-			}
-			writer.write("</table>\n");
-			writer.write("</body>\n");
-			writer.write("</html>");
-			
-			writer.close();
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
+		int n = 5;
+		boolean clockwise = true;
+		
+		Solution solution = new Solution();
+		int[][] result = solution.solution(n,clockwise);
 	}
+}
+
+class Solution {
+    //dir 1 오 2 위 3 왼 4 아래
+    public int[][] solution(int n, boolean clockwise) {
+        int[][] answer = new int[n][n];
+        Queue<Dot> queue = new LinkedList<>();
+        if(clockwise){
+            queue.add(new Dot(0,0,1,1));
+            queue.add(new Dot(0,n-1,4,1));
+            queue.add(new Dot(n-1,n-1,3,1));
+            queue.add(new Dot(n-1,0,2,1));
+
+            int count = 0;
+            
+            while(!queue.isEmpty() && count <= n*n ){
+                Dot d = queue.poll();
+                answer[d.x][d.y] = d.num;
+                if(inbound(d.x,d.y+1,n)&&d.dir == 1 && answer[d.x][d.y+1] != 0){
+                    queue.add(new Dot(d.x+1,d.y,4,d.num+1));
+                }else if(inbound(d.x,d.y+1,n)&&d.dir == 1 && answer[d.x][d.y+1] == 0){
+                    queue.add(new Dot(d.x,d.y+1,d.dir,d.num+1));
+                }else if(inbound(d.x-1,d.y,n)&&d.dir ==2 && answer[d.x-1][d.y] != 0){
+                    queue.add(new Dot(d.x,d.y+1,1,d.num+1));
+                }else if(inbound(d.x-1,d.y,n)&&d.dir ==2 && answer[d.x-1][d.y] == 0){
+                    queue.add(new Dot(d.x-1,d.y,d.dir,d.num+1));
+                }else if(inbound(d.x,d.y-1,n)&&d.dir ==3 && answer[d.x][d.y-1] != 0){
+                    queue.add(new Dot(d.x-1,d.y,2,d.num+1));
+                }else if(inbound(d.x,d.y-1,n)&&d.dir == 3 && answer[d.x][d.y-1] == 0){
+                    queue.add(new Dot(d.x,d.y-1,d.dir,d.num+1));
+                }else if(inbound(d.x+1,d.y,n)&&d.dir == 4 && answer[d.x+1][d.y] != 0){
+                    queue.add(new Dot(d.x,d.y-1,3,d.num+1));
+                }else if(inbound(d.x+1,d.y,n)&&d.dir == 4 && answer[d.x+1][d.y] == 0){
+                    queue.add(new Dot(d.x,d.y-1,3,d.num+1));
+                }
+                count++;
+                
+            }
+        }else{
+
+        }
+        return answer;
+    }
+    public boolean inbound(int x ,int y ,int n){
+        if(x < 0 || x >= n || y < 0 || y >= n) return false;
+        return true;
+    }
+    
+}
+
+class Dot{
+    int x;
+    int y;
+    int dir;
+    int num;
+
+    public Dot(int x, int y, int dir , int num){
+        this.x = x;
+        this.y = y;
+        this.dir = dir;
+        this.num = num;
+    }
 }
--- LJKAlgorithm/Data_Structure/src/VectorTest/Main.java
+++ LJKAlgorithm/Data_Structure/src/VectorTest/Main.java
@@ -1,11 +1,75 @@
 package VectorTest;
 import java.util.*;
 public class Main {
-	
 	public static void main(String[] args) {
-		Vector<Integer> vector =new Vector<>();
-		vector.add(0);
-		vector.get(1);
+		int n = 5;
+		boolean clockwise = true;
 		
+		Solution solution = new Solution();
+		int[][] result = solution.solution(n,clockwise);
 	}
 }
+
+class Solution {
+    //dir 1 ¿À 2 À§ 3 ¿Þ 4 ¾Æ·¡
+    public int[][] solution(int n, boolean clockwise) {
+        int[][] answer = new int[n][n];
+        Queue<Dot> queue = new LinkedList<>();
+        if(clockwise){
+            queue.add(new Dot(0,0,1,1));
+            queue.add(new Dot(0,n-1,4,1));
+            queue.add(new Dot(n-1,n-1,3,1));
+            queue.add(new Dot(n-1,0,2,1));
+
+            int count = 0;
+            
+            while(!queue.isEmpty() && count <= n*n ){
+                Dot d = queue.poll();
+                answer[d.x][d.y] = d.num;
+                if(inbound(d.x,d.y+1,n)&&d.dir == 1 && answer[d.x][d.y+1] != 0){
+                    queue.add(new Dot(d.x+1,d.y,4,d.num+1));
+                }else if(inbound(d.x,d.y+1,n)&&d.dir == 1 && answer[d.x][d.y+1] == 0){
+                    queue.add(new Dot(d.x,d.y+1,d.dir,d.num+1));
+                }else if(inbound(d.x-1,d.y,n)&&d.dir ==2 && answer[d.x-1][d.y] != 0){
+                    queue.add(new Dot(d.x,d.y+1,1,d.num+1));
+                }else if(inbound(d.x-1,d.y,n)&&d.dir ==2 && answer[d.x-1][d.y] == 0){
+                    queue.add(new Dot(d.x-1,d.y,d.dir,d.num+1));
+                }else if(inbound(d.x,d.y-1,n)&&d.dir ==3 && answer[d.x][d.y-1] != 0){
+                    queue.add(new Dot(d.x-1,d.y,2,d.num+1));
+                }else if(inbound(d.x,d.y-1,n)&&d.dir == 3 && answer[d.x][d.y-1] == 0){
+                    queue.add(new Dot(d.x,d.y-1,d.dir,d.num+1));
+                }else if(inbound(d.x+1,d.y,n)&&d.dir == 4 && answer[d.x+1][d.y] != 0){
+                    queue.add(new Dot(d.x,d.y-1,3,d.num+1));
+                }else if(inbound(d.x+1,d.y,n)&&d.dir == 4 && answer[d.x+1][d.y] == 0){
+                    queue.add(new Dot(d.x+1,d.y,3,d.num+1));
+                }
+                count++;
+                
+            }
+        }else{
+
+        }
+        return answer;
+    }
+    public boolean inbound(int x ,int y ,int n){
+        if(x < 0 || x >= n || y < 0 || y >= n) return false;
+        return true;
+    }
+    
+}
+
+class Dot{
+    int x;
+    int y;
+    int dir;
+    int num;
+
+    public Dot(int x, int y, int dir , int num){
+        this.x = x;
+        this.y = y;
+        this.dir = dir;
+        this.num = num;
+    }
+}
+
+
